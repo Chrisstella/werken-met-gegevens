1ab8d393453aed381456a48deb1c9c7aeb8caa92
Chrisstella <99065775@mydavinci.nl>
2021-09-09 09:56:20 +0200

    werken met gegevens

---
+++ feestlunch.py
@@ -0,0 +1,14 @@
+#Feestlunch
+#17 croissantjes van elk 39 eurocent
+#2 stokbroden van elk 2,78 euro kopen.
+#3 kortingsbonnen van 50 eurocent.
+aantal_c = 17.0
+aantal_s = 2.0
+aantal_k = 3.0
+prijs_c = 0.39
+prijs_s = 2.78
+korting_k = 0.50
+
+print(aantal_c * prijs_c + (aantal_s * prijs_s) - (aantal_k * korting_k))
+
+
---
+++ speelhaldag.py
@@ -0,0 +1,13 @@
+#speelhaldag
+#3 vrienden
+#toegangsticket per persoon 7,45
+#met zijn allen voor 45 minuten in de VIP-VR-GameSeat.
+#De VIP-VR GameSeat kost per persoon 37 eurocent per 5 minuten.
+
+aantal_p = 4
+prijs_t = 7.45
+prijs_gs = 0.37
+tijd_op_gs = 45
+tijd_betaal = 5
+
+print(aantal_p * prijs_t + (prijs_gs * (tijd_op_gs / tijd_betaal)))
